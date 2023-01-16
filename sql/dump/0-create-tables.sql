082587487ec69a81c2cf24b262851612501fdae7
Joshua <hi@joshua.cafe>
2023-01-16 14:24:25 +0100

    move foreign key to table creation

--- sql/dump/0-create-tables.sql
+++ sql/dump/0-create-tables.sql
@@ -15,9 +15,7 @@ create table adresse(
     hausnummer varchar(6) not null,
     ref_region_id integer not null,
     primary key(id),
-    foreign key (ref_region_id)
-        references region(id)
-        on delete cascade
+    foreign key (ref_region_id) references region(id) on delete cascade
 );
 
 create table kunde(
@@ -28,31 +26,27 @@ create table kunde(
     telefon varchar(255),
     ref_adresse_id integer not null,
     primary key(id),
-    foreign key (ref_adresse_id)
-        references adresse(id)
-        on delete cascade
+    foreign key (ref_adresse_id) references adresse(id) on delete cascade
 
 );
 
+create table lieferant (
+    id integer not null,
+    lieferantenname varchar(255),
+    ref_adresse_id integer not null,
+    telefon varchar(25),
+    email varchar(50),
+    primary key(id),
+    foreign key (ref_adresse_id) references adresse(id)
+);
 
 create table bestellung(
     id integer not null,
     bestelldatum date,
     ref_kunde_id integer,
     rechnungsbetrag decimal(10,2),
-    primary key(id)
-);
-
-create table bestellungzutat (
-    ref_bestellung_id integer not null,
-    ref_zutat_id integer not null,
-    menge integer not null
-);
-
-create table bestellungrezept (
-    ref_bestellung_id integer not null,
-    ref_rezept_id integer not null,
-    menge integer not null
+    primary key(id),
+    foreign key (ref_kunde_id) references kunde(id)
 );
 
 create table zutat (
@@ -65,8 +59,22 @@ create table zutat (
     kalorien integer,
     kohlenhydrate decimal(10,2),
     protein decimal(10,2),
-    primary key(id)
+    primary key(id),
+    foreign key (ref_lieferant_id)
+        references lieferant(id)
 );
+
+
+create table bestellungzutat (
+    ref_bestellung_id integer not null,
+    ref_zutat_id integer not null,
+    menge integer not null,
+    foreign key (ref_bestellung_id) references bestellung(id),
+    foreign key (ref_zutat_id) references zutat(id)
+);
+
+
+
 
 create table rezept (
     id integer not null,
@@ -77,7 +85,17 @@ create table rezept (
 create table rezeptzutat (
     ref_rezept_id integer not null,
     ref_zutat_id integer not null,
-    menge integer
+    menge integer,
+    foreign key (ref_rezept_id) references rezept(id),
+    foreign key (ref_zutat_id) references zutat(id)
+);
+
+create table bestellungrezept (
+    ref_bestellung_id integer not null,
+    ref_rezept_id integer not null,
+    menge integer not null,
+    foreign key (ref_bestellung_id) references bestellung(id),
+    foreign key (ref_rezept_id) references rezept(id)
 );
 
 create table beschraenkung (
@@ -88,7 +106,10 @@ create table beschraenkung (
 
 create table beschraenkungzutat (
     ref_beschraenkung_id integer not null,
-    ref_zutat_id integer not null
+    ref_zutat_id integer not null,
+    foreign key (ref_beschraenkung_id) references beschraenkung(id),
+    foreign key (ref_zutat_id) references zutat(id)
+
 );
 
 create table ernaehrungskategorie (
@@ -99,14 +120,7 @@ create table ernaehrungskategorie (
 
 create table ernaehrungskategoriezutat (
     ref_ernaehrungskategorie_id integer not null,
-    ref_zutat_id integer not null
-);
-
-create table lieferant (
-    id integer not null,
-    lieferantenname varchar(255),
-    ref_adresse_id integer not null,
-    telefon varchar(25),
-    email varchar(50),
-    primary key(id)
+    ref_zutat_id integer not null,
+    foreign key (ref_ernaehrungskategorie_id) references ernaehrungskategorie(id),
+    foreign key (ref_zutat_id) references zutat(id)
 );
