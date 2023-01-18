3f556b35eb00f053655b31c3bcdd10a692a0131f
Joshua <hi@joshua.cafe>
2023-01-18 10:36:48 +0100

    add web js file

---
+++ web/main.js
@@ -0,0 +1,48 @@
+const API_DOMAIN = "http://localhost:81"
+
+let zutaten_conatiner = document.querySelector('.zutaten__card_container')
+function createZutat({ zutat, kohlenhydrate, protein, kalorien, base, alt }) {
+    return `
+    <div class="zutaten__card">
+        <img class="zutaten__card_img" src="data:image/jpg;base64,${base}" alt="${zutat}">
+        <p class="zutat__name">${zutat}</p>
+        <p class="zutat__nutrition">Durchschnittliche Nährwerte:</p>
+        <table class="zutaten__nutrition_liste">
+            <tbody>
+                <tr>
+                    <th class="zutaten__nutrition_liste_name">Kohlenhydrate:</th>
+                    <td class="zutaten__nutrition_liste_einheit">${kohlenhydrate}kcal</td>
+                </tr>
+                <tr>
+                    <th class="zutaten__nutrition_liste_name">Proteine:</th>
+                    <td class="zutaten__nutrition_liste_einheit">${protein}g</td>
+                </tr>
+                <tr>
+                    <th class="zutaten__nutrition_liste_name">Kalorien:</th>
+                    <td class="zutaten__nutrition_liste_einheit">${kalorien}g</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `
+}
+
+
+async function getAllZutaten() {
+    let zutaten = await $.ajax({
+        type: "GET",
+        url: `${API_DOMAIN}/zutaten`,
+        crossDomain: true
+    })
+    zutaten.forEach(zutat => {
+        $('.zutaten__card_container').append(createZutat({
+            zutat: zutat.bezeichnung,
+            kalorien: parseFloat(zutat.kalorien).toFixed(1),
+            kohlenhydrate: parseFloat(zutat.kohlenhydrate).toFixed(1),
+            protein: parseFloat(zutat.protein).toFixed(1),
+            base: zutat.base
+        }));
+    });
+}
+
+getAllZutaten();
