9d5af4bd29fa32fa677c3f963721b3785435aea5
devmagda <113769198+devmagda@users.noreply.github.com>
2023-01-18 09:12:31 +0100

    Renamed files

--- sql/dump/6-insert-other-data.sql
+++
@@ -1,3 +0,0 @@
-insert into lieferanturl values (101, "https://biohof.de/");
-insert into lieferanturl values (102, "https://www.biohof-lecker.de/");
-insert into lieferanturl values (103, "https://hofkaeserei-hennig.de/");
