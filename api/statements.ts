27dde1578ba70977736a693bcb1d4ffd31a6abf2
Joshua <hi@joshua.cafe>
2023-01-20 14:48:21 +0100

    fix statements in API

--- api/statements.ts
+++ api/statements.ts
@@ -1,5 +1,5 @@
 export default {
-    zutaten: "select z.*, zb.base, zb.alt from zutat z cross join zutatenbilder zb on z.id=zb.ref_zutat_id;",
-    lieferanten: "select LAR.*, url.link from v_liefereanten_adressen_regionen LAR join lieferanturl url on LAR.lieferant_id=url.ref_lieferant_id;",
-    rezepte: "select r.id, r.rezeptname, rb.base from rezept r join rezeptebilder rb on r.id=rb.ref_rezept_id;"
+    zutaten: "select z.*, zb.base, zb.alt from zutat z cross join zutatenbilder zb on z.ref_zutatenbilder_id=zb.id",
+    lieferanten: "select * from v_liefereanten_adressen_regionen;",
+    rezepte: "select r.id, r.rezeptname, rb.base from rezept r join rezeptebilder rb on rb.id=r.ref_rezeptebilder_id;"
 }
